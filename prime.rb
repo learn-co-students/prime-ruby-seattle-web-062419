70032ad3596ef490c34d2c4e8fa3c01c3736f91d
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-05-12 21:45:28 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+    if integer <= 1
+        return false
+    end
+
+    for number in (2..integer/2)
+        mod = integer%number
+        if mod == 0
+            return false
+        end
+    end
+    return true      
+
+end
